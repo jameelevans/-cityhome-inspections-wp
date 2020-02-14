78001abe98e7aa5469e5ef5d4ad2188613f4e71f
Jameel <jameelevans@live.com>
2020-02-13 22:46:29 -0600

    Second commit

--- cityhome-inspections/app/public/wp-content/themes/cityhome-inspections/settings.js
+++
@@ -1,2 +0,0 @@
-
-exports.urlToPreview = 'http://city-home-inspections.local/';
--- cityhome-inspections/app/public/wp-content/themes/cityhome-inspections/template-parts/page-banner.php
+++
@@ -1,9 +0,0 @@
-<div class="page-banner">
-  <h1><?php the_title(); ?></h1>
-  <?php
-    if ( function_exists('yoast_breadcrumb') ) {
-      yoast_breadcrumb( '<p class="page-banner--breadcrumbs">','</p>' );
-    }
-    get_template_part('template-parts/share-button');
-  ?>
-</div>
