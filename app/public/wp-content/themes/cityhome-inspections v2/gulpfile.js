bb0463bf22d1cc273052a35babfdea2e0483cefc
Jameel <jameelevans@live.com>
2020-11-30 23:16:00 -0700

    Added new request inspection page pt 2

--- app/public/wp-content/themes/cityhome-inspections v2/gulpfile.js
+++ app/public/wp-content/themes/cityhome-inspections v2/gulpfile.js
@@ -1,188 +1,55 @@
-/** 
- * Gulpfile for WordPress with Sass
- *
- * ! What the code does:
- * *    1. Live reloads browser with BrowserSync
- * *    2. CSS: Sass to CSS conversion, error catching, Autoprefixing
- * *    3. JS: Utilies Webpack to bundle js modules into one file
- * *    4. Watches files for changes in CSS or JS
- * *    5. Watches files for changes in PHP
- *      
- *  TODO: Update /settings file to fit project 
- *  TODO: Once project settings is set run --> npm install  
- *  TODO: When instalation ends run --> gulp watch 
- *
- */
+var gulp = require('gulp'),
+settings = require('./settings'),
+webpack = require('webpack'),
+browserSync = require('browser-sync').create(),
+postcss = require('gulp-postcss'),
+rgba = require('postcss-hexrgba'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+colorFunctions = require('postcss-color-function');
 
-
-/**
- * * Project variables
- *
- * ! These variables do the following:
- *    1. Load all project settings and plugins
- * 
- */
-
-// * Load project settings 
-const settings = require('./settings'); // File for all project settings ( Edit this to fit project )
-
-// * Load gulp plugins
-const gulp = require('gulp'); // Enables project to use Gulp plugins
-
-// * CSS related plugins
-const sass = require('gulp-sass'); // Gulp plugin for Sass compilation
-const autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic
-
-// * JS related plugins
-const webpack = require('webpack'); // Plugin to concatenate js modules
-
-// * Image related plugins
-const imagemin = require('gulp-imagemin'); // Minify PNG, JPEG, GIF and SVG images with imagemin.
-
-// * Utility related plugins
-const browserSync = require('browser-sync').create(); // Reloads browser and injects CSS. Time-saving synchronized browser testing.
-const notify = require('gulp-notify'); // Sends message notification to you.
-const cache = require('gulp-cache'); // Cache files in stream for later use.
-
-
-/**
- * * Task: styles
- *
- *
- * ! This task compiles sass and autoprefixes css:
- *    1. Gets the source scss file
- *    2. Compiles Sass to CSS
- *    3. Autoprefixes it and generates style.css
- *    4. Injects CSS or reloads the browser via browserSync
- */
-
-gulp.task('styles', () => {
-	return gulp
-		.src(settings.styleSRC)
-		.pipe(
-			sass({
-				errLogToConsole: true,
-				outputStyle: settings.outputStyle,
-				precision: settings.precision
-			})
-		)
-		.pipe(autoprefixer(settings.BROWSERS_LIST))
-		.on('error', (error) => console.log(error.toString()))
-		.pipe(gulp.dest(settings.styleDestination))
-		.pipe(notify({
-			message: '\n\n ✅  Styles task completed ✅\n',
-			onLast: true
-		}));
+gulp.task('styles', function() {
+  return gulp.src('./css/style.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, rgba, colorFunctions, autoprefixer]))
+    .on('error', (error) => console.log(error.toString()))
+    .pipe(gulp.dest('./'));
 });
 
+gulp.task('scripts', function(callback) {
+  webpack(require('./webpack.config.js'), function(err, stats) {
+    if (err) {
+      console.log(err.toString());
+    }
 
-/**
- * * Task: scripts
- *
- *
- * ! This task does the following:
- *     1. Use webpack to bundle js modules
- *	   2. Log errors if there are any
- */
-
-gulp.task('scripts', (callback) => {
-	webpack(require(settings.jsWebpack), function (err, stats) {
-		if (err) {
-			console.log(err.toString());
-		}
-
-		console.log(stats.toString());
-		callback();
-	});
+    console.log(stats.toString());
+    callback();
+  });
 });
 
-/**
- * * Task: images
- *
- *
- * ! This task minifies PNG, JPEG, GIF and SVG images:
- *     1. Gets the source of images raw folder
- *     2. Minifies PNG, JPEG, GIF and SVG images
- *     3. Generates and saves the optimized images
- *
- * This task will run only once, if you want to run it
- * again, do it with the command `gulp images`.
- *
- */
+gulp.task('watch', function() {
+  browserSync.init({
+    notify: false,
+    proxy: settings.urlToPreview,
+    ghostMode: false
+  });
 
-gulp.task('images', () => {
-	return gulp
-		.src(settings.imgSRC)
-		.pipe(
-			cache(
-				imagemin([
-					imagemin.gifsicle({
-						interlaced: true
-					}),
-					imagemin.jpegtran({
-						progressive: true
-					}),
-					imagemin.optipng({
-						optimizationLevel: 3
-					}), // 0-7 low-high.
-					imagemin.svgo({
-						plugins: [{
-							removeViewBox: true
-						}, {
-							cleanupIDs: false
-						}]
-					})
-				])
-			)
-		)
-		.pipe(gulp.dest(settings.imgDST))
-		.pipe(notify({
-			message: '\n\n ✅  Minified images ✅  \n',
-			onLast: true
-		}));
+  gulp.watch('./**/*.php', function(done) {
+    browserSync.reload();
+    done();
+  });
+  gulp.watch('./css/**/*.css', gulp.parallel('waitForStyles'));
+  gulp.watch(['./js/modules/*.js','./js/scripts.js'], gulp.parallel('waitForScripts'));
 });
 
-/**
- * * Task: clearCache
- *
- * ! This task deletes the images cache
- *  By running the "images" task each image will be regenerated.
- * 
- */
-gulp.task('clearCache', (done) => {
-	return cache.clearAll(done);
-});
+gulp.task('waitForStyles', gulp.series('styles', function() {
+  return gulp.src('./style.css')
+    .pipe(browserSync.stream());
+}))
 
-
-/**
- * * Task: watch and waiting for
- *
- * Watches for file changes and runs all other tasks
- * 
- */
-
-gulp.task('watch', () => {
-	browserSync.init({
-		notify: false,
-		proxy: settings.urlToPreview,
-		ghostMode: false
-	});
-
-	gulp.watch(settings.watchPhp, (done) => {
-		browserSync.reload();
-		done();
-	});
-	gulp.watch(settings.watchStyles, gulp.parallel('waitForStyles'));
-	gulp.watch([settings.watchJsModules, settings.watchJsMain], gulp.parallel('waitForScripts'));
-	gulp.watch(settings.imgSRC, gulp.series('images')); // Reload on customJS file changes.
-});
-
-gulp.task('waitForStyles', gulp.series('styles', () => {
-	return gulp.src(settings.styleMain)
-		.pipe(browserSync.stream());
-}));
-
-gulp.task('waitForScripts', gulp.series('scripts', (cb) => {
-	browserSync.reload();
-	cb()
-}));
+gulp.task('waitForScripts', gulp.series('scripts', function(cb) {
+  browserSync.reload();
+  cb()
+}))
--- app/public/wp-content/themes/cityhome-inspections v2/header.php
+++ app/public/wp-content/themes/cityhome-inspections v2/header.php
@@ -1,20 +1,83 @@
 <?php
 /**
- * * The template for displaying the header
+ * Header for the Home page of my theme
  *
- * @package your-wp-project
+ * @package jameelmaterial
  */
 ?>
-	<!doctype html>
-	<html <?php language_attributes(); ?>>
 
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="google-site-verification" content="itLClUYT7mc-qQ-z8NaX_KWF7iSLH-OJ2nkH9SBB3M0" />
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<?php wp_head(); ?>
-	</head>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <header id="top" class="site-header">
+      <div class="site-header_header-top">
+        <div class="wrapper">
+          <a class="site-header_header-top--site-info" href="<?php echo site_url(); ?>">
+            <h1 class="site-header_header-top--logo">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/city-home-inspections-logo.svg" alt="<?php echo get_bloginfo( 'name' ); ?>"> 
+              <span class="site-header_header-top--logo__title"><?php get_bloginfo( 'name' ); ?></span>
+            </h1>
+            <div class="site-header_header-top--description">
+              <span><?php echo get_bloginfo('description'); ?></span>
+            </div>
+          </a>
 
-    <body>
-        Hello from Header
+          <div class="site-header_header-top--contact">
+            <div class="site-header_header-top--contact__detail">
+              <i class="icon icon-clock"></i>
+              <h3>Open Weekdays</h3>
+              <p>8AM - 5:30PM</p>
+            </div>
+            <a href="tel:414-403-7913">
+              <div class="site-header_header-top--contact__detail">
+                <i class="icon icon-phone"></i>
+                <h3>Call Us Now!</h3>
+                <p>414-403-7913</p>
+              </div>
+            </a>
+            <a href="mailto:cityhomeinspec9847@gmail.com">
+              <div class="site-header_header-top--contact__detail">
+                <i class="icon icon-envelope"></i>
+                <h3>Email Us</h3>
+                <p>Send Email</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-header__menu">
+
+          <div class="site-header__menu-icon">
+              <div class="site-header__menu-icon__middle"></div>
+          </div>
+
+        <nav class="site-header__main-navigation">
+          <ul class="wrapper site-header__main-navigation-content">
+            <li <?php if (is_front_page()) echo 'class="current-menu-item"' ?>>
+              <a  class="nav-link" href="<?php if(is_front_page()) {echo '#start-here';}else{echo site_url('/');} ?>">Start Here</a>
+            </li>
+            <li <?php if (is_page('whats-included')) echo 'class="current-menu-item"' ?>>
+              <a  class="nav-link"  href="<?php if(is_front_page()) {echo '#whats-included';}else{echo site_url('/whats-included');} ?>">What's Included?</a>
+            </li>
+            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>>
+              <a  class="nav-link"  href="<?php if(is_front_page()) {echo '#resources';}else{echo site_url('/resources');} ?>">Resources</a>
+            </li>
+            <li <?php if (is_page('our-story')) echo 'class="current-menu-item"' ?>>
+              <a  class="nav-link" href="<?php echo site_url('/our-story') ?>">Our Story</a>
+            </li>
+          </ul>
+
+          <div class="site-header__main-navigation_nav-cta"><a class="btn btn--green-outline" href="<?php if(is_page('request-inspection')) {echo '#';}else{echo site_url('/request-inspection');} ?>">Request an Inspection</a></div>
+
+        </nav>
+      </div>
+    </header>
